1ea9b4baa6a26f203e8569fc32d5193dce9dcc05
Entity2702 <mikigrzywa@gmail.com>
2026-08-24 14:38:55 +0200

    added user schema and implemented user creation feature

---
+++ src/user/user.schema.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { randomUUID } from "crypto";
+import { HydratedDocument } from "mongoose";
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+
+ @Prop()
+ email!: string;
+
+ @Prop()
+ password!: string;
+
+ @Prop({
+  type: String,
+  default: () => randomUUID
+ })
+ id!: string;
+
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
---
+++ src/user/user.service.ts
@@ -0,0 +1,28 @@
+import { ConflictException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './user.schema';
+import { Model } from 'mongoose';
+import { CreateUserDto } from './create-user.dto';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UserService {
+ constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+ async create(createUserDto: CreateUserDto): Promise<User> {
+  const existingUser = await this.userModel.findOne({email: createUserDto.email});
+
+  if(existingUser){
+   throw new ConflictException('User with this email already exists.');
+  }
+  
+  const rounds = 10;
+  const hashedPassword = await bcrypt.hash(createUserDto.password, rounds);
+
+  const createdUser = new this.userModel({...createUserDto, password: hashedPassword});
+  return createdUser.save();
+ }
+
+
+ 
+}
